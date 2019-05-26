4d13a54f61ebeddd0acf411b85294b2dc0315a33
Pavol Varga <pavol.varga@gmail.com>
2019-05-26 19:23:40 +0200

    Disable button if inputs are empty

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,37 @@ import { withRouter } from 'react-router-dom';
 
 class InputForm extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+           selected: {
+               person: undefined,
+               planet: undefined
+           }
+        };
+
+        this.handleOnPersonInputChange = this.handleOnPersonInputChange.bind(this);
+        this.handleOnPlanetInputChange = this.handleOnPlanetInputChange.bind(this);
+    }
+
+    handleOnPersonInputChange(person) {
+        this.setState((state) => ({
+            selected: {...state.selected, person}
+        }));
+    }
+
+    handleOnPlanetInputChange(planet) {
+        this.setState((state) => ({
+            selected: {...state.selected, planet}
+        }));
+    }
+
+    isGenerateBntDisabled() {
+        return !(!!this.state.selected.person && !!this.state.selected.planet);
+    }
+
     render() {
         return (
             <div>
@@ -16,18 +47,34 @@ class InputForm extends Component {
                         <Col>
                             <FormGroup size="lg" row>
                                 <Label for="input-person" size="lg">Character</Label>
-                                <Input id="input-person" name="input-person" placeholder="Please enter a character" size="lg"/>
+                                <Input
+                                    id="input-person"
+                                    name="input-person"
+                                    placeholder="Please enter a character"
+                                    size="lg"
+                                    onChange={e => this.handleOnPersonInputChange(e.target.value)} />
                             </FormGroup>
                         </Col>
                         <Col>
                             <FormGroup size="lg" row>
-                                <Label for="input-planet" size="lg">Character</Label>
-                                <Input id="input-planet" name="input-planet" placeholder="Please enter a planet" size="lg"/>
+                                <Label for="input-planet" size="lg">Planet</Label>
+                                <Input
+                                    id="input-planet"
+                                    name="input-planet"
+                                    placeholder="Please enter a planet"
+                                    size="lg"
+                                    onChange={e => this.handleOnPlanetInputChange(e.target.value)} />
                             </FormGroup>
                         </Col>
                         <Col>
                             <div className="text-center">
-                                <Button color="primary" size="lg" onClick={e => this.props.history.push('/plot')}>Generate Plot</Button>
+                                <Button
+                                    color="primary"
+                                    size="lg"
+                                    onClick={e => this.props.history.push('/plot')}
+                                    disabled={this.isGenerateBntDisabled()}>
+                                    Generate Plot
+                                </Button>
                             </div>
                         </Col>
                     </Form>
