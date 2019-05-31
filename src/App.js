b57bb734c9ddb0b9b9674bec7055f7d9abc4c810
Pavol Varga <pavol.varga@gmail.com>
2019-05-31 17:39:49 +0200

    Show message to a user if initial loading of star wars resources fails

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
-import { Spinner } from 'reactstrap';
+import { Spinner, Alert } from 'reactstrap';
 import { InputForm } from './pages/home/HomePage';
 import { Plot } from './pages/result/ResultPage';
 import { loadStarWarsData } from './common/util';
@@ -34,39 +34,58 @@ class App extends Component {
             data: {
                 people: [],
                 planets: []
-            }
+            },
+            peopleLoadEndedInError: false,
+            planetsLoadEndedInError: false
         };
         this.setPeople = this.setPeople.bind(this);
         this.setPlanets = this.setPlanets.bind(this);
-        this.loadedInitialData = this.loadedInitialData.bind(this);
+        this.isLoadedInitialData = this.isLoadedInitialData.bind(this);
+        this.handlePeopleLoadError = this.handlePeopleLoadError.bind(this);
+        this.handlePlanetsLoadError = this.handlePlanetsLoadError.bind(this);
     }
 
     setPeople(people) {
         this.setState((state) => ({
+            ...state,
             data: {...state.data, people}
         }));
     }
 
     setPlanets(planets) {
         this.setState((state) => ({
+            ...state,
             data: {...state.data, planets}
         }));
     }
 
-    componentDidMount() {
-        //TODO: in case of an error show some info also to a user
-        const reject = (err) => console.error(err);
-
-        loadStarWarsData(RESOURCES.PEOPLE.plural, this.setPeople, reject);
-        loadStarWarsData(RESOURCES.PLANETS.plural, this.setPlanets, reject);
+    handlePeopleLoadError(err) {
+        console.log('Initial loading of people has failed', err);
+        this.setState((state) => ({
+            ...state,
+            peopleLoadEndedInError: true
+        }));
     }
 
-    loadedInitialData() {
+    handlePlanetsLoadError(err) {
+        console.log('Initial loading of planets has failed', err);
+        this.setState((state) => ({
+            ...state,
+            planetsLoadEndedInError: true
+        }));
+    }
+
+    componentDidMount() {
+        loadStarWarsData(RESOURCES.PEOPLE.plural, this.setPeople, this.handlePeopleLoadError);
+        loadStarWarsData(RESOURCES.PLANETS.plural, this.setPlanets, this.handlePlanetsLoadError);
+    }
+
+    isLoadedInitialData() {
         return this.state.data.people.length > 0 && this.state.data.planets.length > 0;
     }
 
     render() {
-        if (!this.loadedInitialData()) {
+        if (!this.state.peopleLoadEndedInError && !this.state.planetsLoadEndedInError && !this.isLoadedInitialData()) {
             return (
                 <div>
                     <Header/>
@@ -74,19 +93,31 @@ class App extends Component {
                 </div>
             );
         }
+        else if (this.state.peopleLoadEndedInError || this.state.planetsLoadEndedInError) {
+            return (
+                <div>
+                    <Header/>
+                    <div className="text-center">
+                        <Alert color="danger">
+                            Loading of initial data failed, please try again later.
+                        </Alert>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            const customProps = {...this.state.data};
+            return (
+                <div>
+                    <Header/>
+                    <Router>
+                        <Route exact path="/" render={(props) => <StarWarsForm {...props} customProps={customProps}/>} />
+                        <Route path="/plot" component={StarWarsPlot}/>
+                    </Router>
+                </div>
 
-        const customProps = {...this.state.data};
-
-        return (
-            <div>
-                <Header/>
-                <Router>
-                    <Route exact path="/" render={(props) => <StarWarsForm {...props} customProps={customProps}/>} />
-                    <Route path="/plot" component={StarWarsPlot}/>
-                </Router>
-            </div>
-
-        );
+            );
+        }
     }
 }
 
