fe74b8b879b11981ed31e5e504a9b63cc3c61f29
Pavol Varga <pavol.varga@gmail.com>
2019-09-23 07:54:54 +0200

    Load boostrap css when mounting application

--- version-B/src/pages/home/HomePage.tsx
+++ version-B/src/pages/home/HomePage.tsx
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import { Form, Container } from 'reactstrap';
 import { RouteComponentProps } from "react-router";
