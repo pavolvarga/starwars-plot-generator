c3a8b207a2ef8e50557dce5d0344f878a3d7e9c1
Pavol Varga <pavol.varga@gmail.com>
2020-10-20 19:29:00 +0200

    Add Home & Result Page (with routing and navigation)

--- version-F/src/app/app-routing.module.ts
+++ version-F/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { ResultPageComponent } from './result/result-page/result-page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'plot', component: ResultPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- version-F/src/app/app.module.ts
+++ version-F/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { ResultPageComponent } from './result/result-page/result-page.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ResultPageComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
