e0dcdc5f6741d3cd55ce2252e3fd4f11966f0442
Pavol Varga <pavol.varga@gmail.com>
2021-01-26 17:54:01 +0100

    Add Header

--- version-G/src/App.vue
+++ version-G/src/App.vue
@@ -1,21 +1,27 @@
 <template>
-  <h1>StarWars Plot Generator</h1>
+  <div class="container-lg text-center">
+    <the-header></the-header>
+  </div>
 </template>
 
 <script>
+import TheHeader from './components/TheHeader';
 
 export default {
-  name: 'App',
+  components: {
+    TheHeader
+  }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ version-G/src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{ header }}</h1>
+  </header>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      header: 'Star Wars Plot Generator'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header {
+    width: 100%;
+  }
+</style>
