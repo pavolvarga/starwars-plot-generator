8c0fe1b40667069a9a1fa5fe61dffc747e5ad4ef
Pavol Varga <pavol.varga@gmail.com>
2021-12-09 18:01:51 +0100

    Add Home & Result pages

---
+++ version-H/components/Layout.tsx
@@ -0,0 +1,19 @@
+import type { NextComponentType } from 'next'
+import Head from 'next/head'
+
+export const Layout: NextComponentType = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <title>SW Gen Version H</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto">
+        {children}
+      </div>
+    </div>
+  )
+}
--- version-H/pages/result.tsx
+++ version-H/pages/result.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next'
 import { Layout } from '@/components/Layout';
 
-const HomePage: NextPage = () => {
+const ResultPage: NextPage = () => {
   return (
     <Layout>
       <div className="flex justify-center">
-        <h1>SW Gen Version H / HomePage</h1>
+        <h1>SW Gen Version H / ResultPage</h1>
       </div>
     </Layout>
   )
 }
 
-export default HomePage
+export default ResultPage
