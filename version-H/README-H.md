6cdc87233baff00cc7f593b330792a6eff4aeccc
Pavol Varga <pavol.varga@gmail.com>
2022-02-08 18:27:48 +0100

    Load data from swapi at application start

--- version-H/README-H.md
+++
@@ -1,8 +0,0 @@
-# starwars-plot-generator
-Web application for generating new star wars plot
-
-## Used technologies
-  * typescript
-  * next.js
-  * tailwindcss
-  
--- version-H/pages/index.tsx
+++ version-H/pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
 import { Layout } from '@/components/Layout';
 import {InputForm} from '@/components/InputForm';
+import {loadStarWarsData} from '@/common/load-data';
+import {Resources} from '@/common/types';
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = ({ resources }) => {
+  console.log('fe', resources);
   return (
     <Layout>
       <div className="flex flex-col items-center justify-center">
@@ -11,6 +14,31 @@ const HomePage: NextPage = () => {
       </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+
+  const resources: Resources = {
+    person:   { plural: 'people',    singular: 'person',   mandatory: true,  label: 'character', suggestions: null, },
+    planet:   { plural: 'planets',   singular: 'planet',   mandatory: true,  label: null, suggestions: null  },
+    starship: { plural: 'starships', singular: 'starship', mandatory: false, label: null, suggestions: null  },
+    vehicle:  { plural: 'vehicles',  singular: 'vehicle',  mandatory: false, label: null, suggestions: null  },
+    species:  { plural: 'species',   singular: 'species',  mandatory: false, label: null, suggestions: null  }
+  };
+
+  const data = await Promise.all(
+    Object.values(resources).map(r => r.plural).map(loadStarWarsData)
+  );
+
+  Object.values(resources).forEach((r, i) => {
+    r.suggestions = data[i];
+  })
+
+  return {
+    props: {
+      resources,
+    },
+  };
 }
 
 export default HomePage
