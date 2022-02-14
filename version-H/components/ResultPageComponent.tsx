7676b3d22b21db82b0037aad5953ddcf21e03469
Pavol Varga <pavol.varga@gmail.com>
2022-02-14 20:58:41 +0100

    Show generated plot on the result page

--- version-H/components/ResultPageComponent.tsx
+++ version-H/components/ResultPageComponent.tsx
@@ -1,26 +1,50 @@
 import React, {FC} from 'react';
 import { Layout } from '@/components/Layout';
 import classNames from 'classnames';
+import DOMPurify from 'isomorphic-dompurify';
 import {useRouter} from 'next/router';
 import {useAppDispatch, useAppState} from '../context/appContext';
+import {generatePlot} from '@/common/plotGenerator';
 
 const ResultPageComponent: FC = () => {
   const router = useRouter();
   const appStateDispatch = useAppDispatch();
   const appState = useAppState();
-  console.log('!!', appState);
+
+  const names = Object.values(appState).map(v => v?.name);
+  const resources = Object.values(appState).map(v => v?.url);
+  const {title, description} = generatePlot(...(names as [string, string, string?, string?, string?]));
+  const markup = {__html: DOMPurify.sanitize(description)};
+
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-center text-4xl my-8">Star Wars Plot Generator</h1>
+      <div className="flex flex-col items-center justify-center my-8">
+        <h1 className="text-center text-4xl">Star Wars Plot Generator</h1>
       </div>
-      <div className="flex justify-center">
+      <div className="flex flex-col items-center justify-center my-6">
+        <h1 className="text-center text-3xl">{title}</h1>
+      </div>
+      <div className="flex flex-col items-center justify-center my-4">
+        <p className="text-center text-xl bg-black text-yellow-200">
+          <span dangerouslySetInnerHTML={markup}/>
+        </p>
+      </div>
+      <div className="flex flex-col items-center justify-center my-4">
+        <h1 className="text-center text-3xl my-2">Resources</h1>
+        <ul className="my-2">
+          {resources.map((url) => (
+            <li className="my-1"><a className="text-blue-600" href={url}>{url}</a></li>)
+          )}
+        </ul>
+      </div>
+      <div className="flex items-center justify-center my-4">
         <button
           className={classNames("text-white text-xl mx-4 p-3 rounded-md", 'bg-blue-500')}
           type="button"
           onClick={() => {
-            appStateDispatch({ type: 'RESET_APP_STATE', payload: undefined });
-            router.push('/');
+            router.push('/').then(() => {
+              appStateDispatch({ type: 'RESET_APP_STATE', payload: undefined });
+            });
           }}
         >
           {'Generate New Plot'}
